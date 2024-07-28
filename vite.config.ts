6f406cc24638db23794e9e8ffbc76d4da85b469e
EvegeniyNekrasov <eanekrasovart@gmail.com>
2024-07-28 16:55:32 +0200

    feat: format configs

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [react()],
     resolve: {
-        alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+        alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
     },
     test: {
         globals: true,
