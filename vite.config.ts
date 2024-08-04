73e6e2348cc9c3a89c653ac0dcc3e34a7064dd2f
EvegeniyNekrasov <eanekrasovart@gmail.com>
2024-08-04 11:22:03 +0200

    feat: changing path import in vite.config.ts and changing the imports order

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
+import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
